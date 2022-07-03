97d4e5af97dc412a11da7ac03082eafc79d6657d
Alex Maksimov <almaximort@yandex.ru>
2022-07-04 01:56:39 +0300

    Inital version of reading from rom example

--- read_file_from_rom/hello/src/hello.c
+++ read_file_from_rom/hello/src/hello.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, const char *argv[])
 {
@@ -11,6 +13,27 @@ int main(int argc, const char *argv[])
         for(int i=1;i<argc;i++)
           fprintf(stderr," Argv = %s\n",argv[i]);
     }
+
+    // Read file
+    int fd;
+
+    fd = open("/foo.cfg",O_RDONLY);
+
+    if(fd<0) {
+        fprintf(stderr,"Can't open foo.cfg\n"); 
+    }
+
+    char buf[1000];
+    int cnt = read(fd,buf,100);
+
+    if (cnt>0) {
+        buf[10] = 0;
+        fprintf(stderr,"Read: %s\n",buf);
+    }
+    else {
+        fprintf(stderr,"Can't read foo.cfg\n"); 
+    }
+    close(fd);
 
     return EXIT_SUCCESS;
 }
