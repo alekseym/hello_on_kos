e186e6f2d865efbe6150e5b6931f7e43642d45bf
Alex Maksimov <almaximort@yandex.ru>
2022-07-03 23:48:53 +0300

    Hello world with arguments

--- hello/src/hello.c
+++ hello/src/hello.c
@@ -5,7 +5,12 @@
 int main(int argc, const char *argv[])
 {
     // no dependency from vfs_entity when using stderr
-    fprintf(stderr,"Hello world!\n");
+    fprintf(stderr,"Hello world! argc = %d\n",argc); 
+
+    if(argc>1) {
+        for(int i=1;i<argc;i++)
+          fprintf(stderr," Argv = %s\n",argv[i]);
+    }
 
     return EXIT_SUCCESS;
 }
